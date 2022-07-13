5533b0670099b31767e8900046c7e8b4e42b1c1a
DimS <dimsed86@gmail.com>
2022-07-14 02:47:37 +0300

    Домашняя работа 4 по предмету 'ВЭБ-технологии'

---
+++ gb_hw4.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "tablehw4" (
+	"id"	INTEGER,
+	"Name"	TEXT,
+	"age"	INTEGER,
+	"address"	TEXT,
+	PRIMARY KEY("id"));
+CREATE INDEX "" ON "tablehw4" (
+	"id"
+);
+INSERT INTO tablehw4 VALUES(0001, "Андрей Гоптарев", 33, "г. Москва");
+INSERT INTO tablehw4 VALUES(0002, "Юлия Kравченко", 18, "г. Санкт-Петербург");
+INSERT INTO tablehw4 VALUES(0003, "Виктор Егоров", 29, "г. Ульяновск");
+INSERT INTO tablehw4 VALUES(0004, "ВладиславНиколаевич Безусов", 20, "г. Москва");
+INSERT INTO tablehw4 VALUES(0005, "Павел Палагин", 32, "г. Казань");
+INSERT INTO tablehw4 VALUES(0006, "Ирина Видилина", 31, "г. Новосибирск");
+INSERT INTO tablehw4 VALUES(0007, "Игорь Черенков", 46, "г. Воронеж");
+INSERT INTO tablehw4 VALUES(0008, "Газим Керимов", 27, "г. Санкт-Петербург");
+INSERT INTO tablehw4 VALUES(0009, "Igor Shylo", 46, "г. Санкт-Петербург");
